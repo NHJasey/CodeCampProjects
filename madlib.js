6d08db4ff5e3b3f3946a3370437b356f6326e7a9
Jasey <nhjasey@gmail.com>
2016-06-14 14:52:43 -0400

    Create madlib.js

---
+++ madlib.js
@@ -0,0 +1,9 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+ 
+  result = "# " + myNoun + " # " + myAdjective + " # " + myVerb + " # " + myAdverb + "."
+
+  return result;
+}
+
+wordBlanks("#dog", "#big", "#runs", "#slowly");
