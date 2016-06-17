08b823b9404ff5af9a683fbcfa872fe0e8a71e30
Jasey <nhjasey@gmail.com>
2016-06-17 17:51:51 -0400

    Create reversestring.js

---
+++ reversestring.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+    var splitString = str.split(""); 
+   
+    var reverseArray = splitString.reverse();
+ 
+    var joinArray = reverseArray.join("");
+   
+    return joinArray;
+}
+ 
+reverseString("#hello from Jasey");
